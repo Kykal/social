c646813d38d112f43f7c582e3262f78d21c12ff6
Alan Benavides <54295964+Kykal@users.noreply.github.com>
2022-01-23 06:09:25 -0600

    Suspense component removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 
 //Stylesheets
 import './App.css';
@@ -10,7 +10,7 @@ import primaryLinks from './Data/primary_links.json';
 import secondaryLinks from './Data/secondary_links.json';
 
 //Mantine
-import { Center, Grid, Image, LoadingOverlay, MantineProvider } from '@mantine/core';
+import { Center, Grid, Image, MantineProvider } from '@mantine/core';
 
 //Custom components
 import PrimaryBanner from './Components/PrimaryBanner/PrimaryBanner';
@@ -19,37 +19,35 @@ import SecondaryBanner from './Components/SecondaryBanner/SecondaryBanner';
 //Component content
 const App = () => {
    return (
-      <Suspense fallback={<LoadingOverlay visible={true} />} >
-         <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles >
-            <header>
-               <Grid justify="center" align="center" >
-                  <Grid.Col xs={6} >
-                     <Center>
-                        <Image radius="xl" height={120} caption={`@${myData.username}`} src={profilePicture}  />
-                     </Center>
+      <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles >
+         <header>
+            <Grid justify="center" align="center" >
+               <Grid.Col xs={6} >
+                  <Center>
+                     <Image radius="xl" height={120} caption={`@${myData.username}`} src={profilePicture}  />
+                  </Center>
+               </Grid.Col>
+            </Grid>
+         </header>
+         <main>
+            <Grid justify="center" align="center"  >
+               {primaryLinks.map((link, index) => (
+                  <Grid.Col key={index} span={12} className='custom-button-grid' >
+                     <PrimaryBanner data={link}/>
                   </Grid.Col>
-               </Grid>
-            </header>
-            <main>
-               <Grid justify="center" align="center"  >
-                  {primaryLinks.map((link, index) => (
-                     <Grid.Col key={index} span={12} className='custom-button-grid' >
-                        <PrimaryBanner data={link}/>
+               ))}
+            </Grid>
+            <Grid justify="center" align="center" grow className="secondary-links" >
+               <Grid span={12} grow justify="center" align="center" >
+                  {secondaryLinks.map((link, index) => (
+                     <Grid.Col key={index} span={1} >
+                        <SecondaryBanner data={link} />
                      </Grid.Col>
                   ))}
                </Grid>
-               <Grid justify="center" align="center" grow className="secondary-links" >
-                  <Grid span={12} grow justify="center" align="center" >
-                     {secondaryLinks.map((link, index) => (
-                        <Grid.Col key={index} span={1} >
-                           <SecondaryBanner data={link} />
-                        </Grid.Col>
-                     ))}
-                  </Grid>
-               </Grid>
-            </main>
-         </MantineProvider>
-      </Suspense>
+            </Grid>
+         </main>
+      </MantineProvider>
    );
 };
 
